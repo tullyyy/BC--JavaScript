963fc495a484d4a27704566a45c2a9cb3737bdc7
Tully <sehtully@gmail.com>
2020-03-06 10:07:27 +0100

    BCDay20-Sidebar
    JS sidebar with transformations. CSS.

--- MW/index.js
+++ MW/index.js
@@ -1,38 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const padlock = document.querySelector(".padlock");
-  const text = document.querySelector(".text");
+  const aside = document.querySelector(".fullSidebar");
 
-  padlock.addEventListener("mouseleave", () => {
-    text.style.display = "none";
+  aside.addEventListener("mouseenter", () => {
+    // document.querySelector(".fullSidebar").classList.add("active");
+    // document.querySelector(".menu").classList.add("active");
+    document.querySelector(".slider").classList.add("active");
   });
-  padlock.addEventListener("mouseenter", () => {
-    text.style.display = "block";
+  aside.addEventListener("mouseleave", () => {
+    document.querySelector(".fullSidebar").classList.remove("active");
+    // document.querySelector(".menu").classList.remove("active");
+    document.querySelector(".slider").classList.remove("active");
   });
 });
-
-// mouseenter           text is diplayed
-
-// mouseleave           text is hidden
-
-// Attach these events to the padlock element on the document's DOMContentLoaded event inside your index.js.
-
-// Martin's solution
-// document.addEventListener('DOMContentLoaded', () => {
-// const padlock = document.querySelector('#padlock');
-// const message = document.querySelector('#message');
-
-// padlock.addEventListener("mouseleave", () => {
-//   message.className = 'message hidden'
-// OR use message.classList.add('hidden')
-// });
-// padlock.addEventListener("mouseenter", () => {
-//   message.className = 'message'
-// >>OR use message.classList.remove('hidden')
-// >>OR use message.classList.toggle('hidden')
-// });
-// });
-
-// Toggle will remove/add on/off> from previous state to other state.
-
-// IN CSS:
-// .hidden {opacity: 0;}
