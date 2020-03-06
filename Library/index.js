48fb338198fe006da36dccf66b312664a79b1c21
Tully <sehtully@gmail.com>
2020-03-06 10:10:31 +0100

    BCDay20-Library
    Adding and removing items from a library (list). JS & CSS.

---
+++ Library/index.js
@@ -0,0 +1,43 @@
+// The public library
+// Let's practice representing data in our web application. We will create a simple library website for which we need an object representing a book.
+
+// Stage 1:
+// Design an object which will represent a book in a public library. The book should have at least these properties
+// title
+// it should allow for multiple authors
+// number of pages
+// publish year
+// whether the book can be borrowed home or can be only viewed in the library
+// whether the book is borrowed or present in the library.
+// Once you have the structure, pick three or four favorite books of yours and create an array of them.
+// Make a simple (but nice) website which displays the list of these books in some way.
+// Stage 2:
+// At the bottom of the page create a form which will allow the library administrator to add a new book to the library.
+// In the list of books add a button which will allow the user to borrow the book it it is not yer borrowed by somebody else.
+
+
+const library = [{
+  title: 'The Big Bad Wolf',
+  author: [{
+    'Jauty McFry',
+  } {
+    'Jauty McFry'
+  }]
+  pages: 145,
+  year: 1765,
+}, {
+  title: 'Sad Mad Happy Puppy',
+  author: 'Lookie Bookfly',
+  pages: 15,
+  year: 1264,
+}, {
+  title: 'My Little Homefry',
+  author: 'Sally',
+  pages: 1415,
+  year: 1946,
+}, {
+  title: 'Everything is Nothing',
+  author: 'Deanne Lima',
+  pages: 445,
+  year: 1999,
+}]
