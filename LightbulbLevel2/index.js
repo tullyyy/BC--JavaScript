a1534d55bb5a0f967b1ee9663b325f85b3bce71b
Tully <sehtully@gmail.com>
2020-03-12 09:53:57 +0100

    BCDay19-Lightbulb Level 2
    JS lightbulbs on and off.
    JS & CSS.

---
+++ LightbulbLevel2/index.js
@@ -0,0 +1,26 @@
+render()
+
+mount()
+
+
+
+
+
+
+const bulb = document.querySelector('#bulb');
+bulb.classList.toggle("bulb-on");
+
+const switchBulb = () => {
+  const bulb = document.querySelector('#bulb');
+  bulb.classList.toggle("bulb-on");
+}
+
+// Take the solution the lightbulb exercise from day 13 and turn the lightbulb into a component. 
+
+// Properly implement the render method to create the lightbulb's DOM element,
+
+// themount method to mount it on the page, 
+
+// and the update method to make the bulb switch between ON/OFF. 
+
+// The state of the component can be represented as a single boolean value with true for ON and false for OFF.
